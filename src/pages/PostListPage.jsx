0ee7899d375a4ef51926f3146fc77e3c70ee4d44
FrancescoGiommi <francescogiommi97@icloud.com>
2024-12-10 15:05:19 +0100

    Add post list

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function PostListPage() {
   return (
     <>
       <main>
         <div className="container mt-5">
           <h1>PostList</h1>
+          <ul>
+            <li>
+              <Link to="/post/1">Post 1</Link>
+            </li>
+            <li>
+              <Link to="/post/2">Post 2</Link>
+            </li>
+            <li>
+              <Link to="/post/3">Post 3</Link>
+            </li>
+            <li>
+              <Link to="/post/4">Post 4</Link>
+            </li>
+          </ul>
         </div>
       </main>
     </>
